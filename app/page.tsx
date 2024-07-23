40d3bac187cb43ce3c0350a26921e643d03f2473
Liem Gearen <liem.gearen1996@gmail.com>
2024-07-23 15:36:17 -0500

    added ci, naming

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,10 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+        <h1 className={title()}>Welcome to&nbsp;</h1>
+        <h1 className={title({ color: "green" })}>Hen House</h1>
+        <h2 className={subtitle()}>
+          The best place to start for tracking your chickens and egg production.
         </h2>
       </div>
 
